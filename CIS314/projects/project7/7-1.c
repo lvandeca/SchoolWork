ef8439c78921b058914970bf757fe597e63fb392
Luke Vandecasteele <lvandeca@uoregon.edu>
2021-03-04 23:46:34 -0800

    documentation

--- CIS314/projects/project7/7-1.c
+++ CIS314/projects/project7/7-1.c
@@ -1,22 +1,25 @@
 /*
-
+Author: Luke Vandecasteele
+Date: 3/3/2021 Last Modified: 3/4/2021
+Credits: Class notes and powerpoint
+Description: Breaking down bits of an address using bitwise operations
 */
 
 #include <stdio.h>
 
 unsigned int getOffset(unsigned int address)
 {
-    return address & 0xFF;
+    return address & 0xFF; //get bottom 8 bits
 }
 
 unsigned int getSet(unsigned int address)
 {
-    return (address >> 8) & 0xF;
+    return (address >> 8) & 0xF; //shift to get rid of offset bits and then git 4 bits
 }
 
 unsigned int getTag(unsigned int address)
 {
-    return address >> 12;
+    return address >> 12; //shift to remove bottom 12 bits and then get remaining for tag
 }
 
 int main()
@@ -26,5 +29,5 @@ int main()
            getTag(0x12345678), getSet(0x12345678));
     printf("0x87654321\n");
     printf("Offset: %x - Tag: %x - Set: %x\n", getOffset(0x87654321),
-            getTag(0x87654321), getSet(0x87654321));
+           getTag(0x87654321), getSet(0x87654321));
 }
--- CIS314/projects/project7/7-3c.c
+++ CIS314/projects/project7/7-3c.c
@@ -1,5 +1,15 @@
+/*
+Author: Luke Vandecasteele
+Date: 3/3/2021 Last Modified: 3/4/2021
+Credits: Class
+notes and powerpoints
+Description: Optimizing code by analyzing memory lookups and limiting memory
+             access by ensuring that larger blocks are available in our cache 
+             for accessing.
+*/
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 struct ColorPoint
 {
@@ -43,13 +53,66 @@ long g(struct ColorPoint **points, int n)
 
 struct ColorPoint **create2DArray(int n)
 {
-    //Array to hold a pointer to the beginning of each row
+    // Array to hold a pointer to the beginning of each row
     struct ColorPoint **points =
-        (struct ColorPoint **)malloc(n * sizeof(struct Colorpoint *));
+        (struct ColorPoint **)malloc(n * sizeof(struct ColorPoint *));
     for (int i = 0; i < n; ++i)
     {
-        //Array to hold each row
+        // Array to hold each row
         points[i] =
-            (struct Colorpoint *)malloc(n * sizeof(struct Colorpoint *));
+            (struct ColorPoint *)malloc(n * sizeof(struct ColorPoint));
+        for (int j = 0; j < n; ++j)
+        {
+            // Init the ColorPoint struct
+            points[i][j].a = rand();
+            points[i][j].r = rand();
+            points[i][j].g = rand();
+            points[i][j].b = rand();
+        }
     }
+    return points;
+}
+void free2DArray(struct ColorPoint **points, int n)
+{
+    for (int i = 0; i < n; ++i)
+    {
+        free(points[i]);
+    }
+    free(points);
+}
+
+int main()
+{
+    struct ColorPoint **points = create2DArray(2048);
+    clock_t start;
+    clock_t end;
+    double seconds;
+
+    //test time for f()
+    start = clock();
+    int sum = f(points, 2048);
+    end = clock();
+
+    seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("f() sum: %d ", sum);
+    printf("time f(): %f\n", seconds);
+
+    //test time for g()
+    start = clock();
+    sum = g(points, 2048);
+    end = clock();
+
+    seconds = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("f() sum: %d ", sum);
+    printf("time g(): %f\n", seconds);
+
+    free2DArray(points, 2048);
+
+    /*
+    The reason that functin g() is so much faster is simply because we are
+    making less memory access. As we have learned, accessing our cache is much
+    faster than going into memory and finding an element. Therefore, since we
+    have fewer misses in the cache, then our code accessing memory less, and
+    becomes faster!
+    */
 }
